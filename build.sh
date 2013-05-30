b259d2417c37093060aafcb20366ac4feaf09298
FeepingCreature <default_357-line@yahoo.de>
2013-05-30 19:03:17 +0200

    Added 'How to go from device coordinates back to worldspace in OpenGL (with explanation)'

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ do
 		cat template_post
 	) > ${FILE/%.md/.html}
 done
+ls *.tex |while read FILE
+do
+        TITLE=$(head -n 1 "$FILE")
+        (
+                tail -n +2 "$FILE" |\
+                  sed -e "s/%TITLE%/$TITLE/g" |\
+                  pandoc --self-contained -s --webtex -f latex -t html
+        ) > ${FILE/%.tex/.html}
+done
